38dd77267f0179c312dd6656fa9db1db4e9236f3
Mayank Bhatt <mak.2901@gmail.com>
2018-08-07 13:45:39 +0530

    socket events

--- socket/chat.js
+++ socket/chat.js
@@ -28,76 +28,93 @@ module.exports = function(io) {
 	io.on('connection', function(socket){
 		socket.on('message', function(data){
 			console.log('data',data)
-			if(data.new_message){
-				const conversation = new Conversation({
-				    participants: [req.user._id, req.params.recipient]
-				});
+			redis.get(data.token, function (err, reply) {
+		    	if(err){
+					console.log('err:',err)
+					response.error = true;
+					response.code = 500;
+					response.userMessage = "There was a problem with the request, please try again."
+					response.data = null
+					response.errors = err
+					socket.emit('auth_err',response)
+				}
+				else{
+					if(data.new_message){
+						const conversation = new Conversation({
+						    participants: [req.user._id, req.params.recipient]
+						});
 
-				conversation.save(function(err, newConversation) {
-				    if (err) {
-				      res.send({ error: err });
-				      return next(err);
-				    }
+						conversation.save(function(err, newConversation) {
+						    if (err) {
+						      	response.error = true;
+						        response.code = 500;
+						        response.userMessage = 'Error Occured';
+						        response.data = null
+						        response.errors = null;
+						        console.log('err',response)
+						        socket.emit('msg_err',response)
+						    }
 
-				    const message = new Message({
-				      conversationId: newConversation._id,
-				      body: req.body.composedMessage,
-				      author: req.user._id
-				    });
+						    const message = new Message({
+						      conversationId: newConversation._id,
+						      body: req.body.composedMessage,
+						      author: req.user._id
+						    });
 
-				    message.save(function(err, newMessage) {
-				      	if (err) {
-				        	response.error = true;
-					        response.code = 500;
-					        response.userMessage = 'Error Occured';
-					        response.data = null
-					        response.errors = null;
-					        console.log('err',response)
-					        return SendResponse(res, 500);
-				      	}
-				      	else{
-				      		response.error = false;
-					        response.code = 200;
-					        response.userMessage = 'Conversation started!';
-					        response.data = {
-					        	conversationId: conversation._id
-					        };
-					        response.errors = null;
-					        console.log('response',response)
-					        return SendResponse(res, 200);
-					    }
-				    });
-				});
-			}
-			else{
-				const reply = new Message({
-				    conversationId: req.params.conversationId,
-				    body: req.body.composedMessage,
-				    author: req.user._id
-				});
-
-				reply.save(function(err, sentReply) {
-				    if (err) {
-				    	response.error = true;
-				        response.code = 500;
-				        response.userMessage = 'Error Occured';
-				        response.data = null
-				        response.errors = null;
-				        console.log('err',response)
-				        return SendResponse(res, 500);
-				    }
-				    else{
-				    	response.error = false;
-				        response.code = 200;
-				        response.userMessage = 'Reply successfully sent!';
-				        response.data = null
-				        response.errors = null;
-				        console.log('response',response)
-				        return SendResponse(res, 200);
+						    message.save(function(err, newMessage) {
+						      	if (err) {
+						        	response.error = true;
+							        response.code = 500;
+							        response.userMessage = 'Error Occured';
+							        response.data = null
+							        response.errors = null;
+							        console.log('err',response)
+							        socket.emit('msg_err',response)
+						      	}
+						      	else{
+						      		response.error = false;
+							        response.code = 200;
+							        response.userMessage = 'Conversation started!';
+							        response.data = {
+							        	conversationId: conversation._id
+							        };
+							        response.errors = null;
+							        console.log('response',response)
+							        socket.emit('msg_success',response)
+							    }
+						    });
+						});
 					}
-				});
-			}
-		});
+					else{
+						const reply = new Message({
+						    conversationId: req.params.conversationId,
+						    body: req.body.composedMessage,
+						    author: req.user._id
+						});
 
+						reply.save(function(err, sentReply) {
+						    if (err) {
+						    	response.error = true;
+						        response.code = 500;
+						        response.userMessage = 'Error Occured';
+						        response.data = null
+						        response.errors = null;
+						        console.log('err',response)
+						        socket.emit('msg_err',response)
+						    }
+						    else{
+						    	response.error = false;
+						        response.code = 200;
+						        response.userMessage = 'Reply successfully sent!';
+						        response.data = null
+						        response.errors = null;
+						        console.log('response',response)
+						        socket.emit('msg_success',response)
+							}
+						});
+					}
+				}
+			})
+		})
 	})
 }
